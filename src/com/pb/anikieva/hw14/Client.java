dc69a2d87624807529598b60790e805854c49921
Daria Anikieva <darja.anikieva@privatbank.ua>
2022-01-05 13:42:02 +0200

    init commit

--- src/com/pb/anikieva/hw14/Client.java
+++ src/com/pb/anikieva/hw14/Client.java
@@ -1,108 +1,36 @@
 package com.pb.anikieva.hw14;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.time.LocalDateTime;
-
-
-class Client1 {
-
-    private Socket socket;
-    private BufferedReader in;
-    private BufferedWriter out;
-    private BufferedReader inputUser;
-    private String addr;
-    private int port;
-    private static LocalDateTime ldt = LocalDateTime.now();
-
-
-    public Client1(String addr, int port) {
-        this.addr = addr;
-        this.port = port;
-        try {
-            this.socket = new Socket(addr, port);
-        } catch (IOException e) {
-            System.err.println("Socket failed");
-        }
-        try {
-            inputUser = new BufferedReader(new InputStreamReader(System.in));
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            new ReadMsg().start();
-            new WriteMsg().start();
-        } catch (IOException e) {
-
-            Client1.this.downService();
-        }
-
-    }
-
-    private void downService() {
-        try {
-            if (!socket.isClosed()) {
-                socket.close();
-                in.close();
-                out.close();
-            }
-        } catch (IOException ignored) {}
-    }
-
-
-    private class ReadMsg extends Thread {
-        @Override
-        public void run() {
-
-            String str="first";
-            try {
-                while (true) {
-                    str = in.readLine();
-                    if (str.equals("stop")) {
-                        Client1.this.downService();
-                        break;
-                    }
-                    System.out.println(str);
-                }
-            } catch (IOException e) {
-                Client1.this.downService();
-            }
-        }
-    }
-
-
-    public class WriteMsg extends Thread {
-
-        @Override
-        public void run() {
-            while (true) {
-                String userWord;
-                try {
-
-                    userWord = inputUser.readLine();
-                    if (userWord.equals("stop")) {
-                        out.write("stop" + "\n");
-                        Client1.this.downService();
-                        break;
-                    } else {
-                        out.write( ldt.toString()+":"+userWord + "\n");
-                    }
-                    out.flush();
-                } catch (IOException e) {
-                    Client1.this.downService();
-
-                }
-
-            }
-        }
-    }
-}
 
 public class Client {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Старт клиента");
+        String serverIp = "127.0.0.1";
+        int serverPort = 1234;
+        System.out.println("Соединяемся с сервером " + serverIp + ":" + serverPort);
 
-    public static String ipAddr = "127.0.0.1";
-    public static int port = 1234;
+        Socket server = new Socket(serverIp, serverPort);
+        BufferedReader inServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        PrintWriter outServer = new PrintWriter(server.getOutputStream(), true);
+        BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        new Client1(ipAddr, port);
+        String dataFromUser, dataFromServer;
+
+        // Основной цикл отправки сообщений серверу
+        while ((dataFromUser = inConsole.readLine()) != null) {
+            outServer.println(dataFromUser);
+            dataFromServer = inServer.readLine();
+            System.out.println(dataFromServer);
+            if ("exit".equalsIgnoreCase(dataFromUser)) {
+                break;
+            }
+        }
+        outServer.close();
+        inServer.close();
+        outServer.close();
+        server.close();
     }
-
 }
--- src/com/pb/anikieva/hw14/Server.java
+++ src/com/pb/anikieva/hw14/Server.java
@@ -1,88 +1,70 @@
 package com.pb.anikieva.hw14;
 
 import java.io.*;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.time.LocalDateTime;
-import java.util.LinkedList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-class Server1 extends Thread {
-    private Socket socket;
-    private BufferedReader in;
-    private BufferedWriter out;
-    private static LocalDateTime ldt = LocalDateTime.now();
-
-    public Server1(Socket socket) throws IOException {
-        this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        start();
-    }
-    @Override
-    public void run() {
-        String word;
-        try{
-            try {
-                while (true) {
-                    word = in.readLine();
-                    if(word.equals("stop")) {
-                        this.downService();
-                        break;
-                    }
-                    System.out.println( word);
-
-                    for (Server1 vr : Server.serverList) {
-                        vr.send(word);
-                    }
-                }
-            } catch (NullPointerException ignored) {}
-        } catch (IOException e) {
-            this.downService();
-        }
-    }
-
-    private void send(String msg) {
-        try {
-            out.write(ldt.toString()+msg + "\n");
-            out.flush();
-        } catch (IOException ignored) {}
-
-    }
-
-    private void downService() {
-        try {
-            if(!socket.isClosed()) {
-                socket.close();
-                in.close();
-                out.close();
-            }
-        } catch (IOException ignored) {}
-    }
-}
 
 public class Server {
 
-    public static final int PORT = 1234;
+    static class Handler implements Runnable {
+        private final Socket clientSocket;
 
-    public static LinkedList<Server1> serverList = new LinkedList<>();
+        public Handler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(PORT);
-        System.out.println("Сервер запущен");
-        try {
-            while (true) {
-                Socket socket = server.accept();
+        @Override
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                String clientMessage;
+
+                // цикл ожидания сообщений от клиента
+                System.out.println("Ожидаем отклика");
+                while ((clientMessage = in.readLine()) != null) {
+                    if ("exit".equalsIgnoreCase(clientMessage)) {
+                        break;
+                    }
+                    out.println("Сервер: " + LocalDateTime.now().withNano(0) + " "  + clientMessage);
+                    System.out.println(Thread.currentThread().getName() + " " + clientMessage);
+                }
+
+                out.close();
+                in.close();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
                 try {
-
-                    System.out.println("Новое соединение.");
-                    serverList.add(new Server1(socket));
-                } catch (IOException e) {
-
-                    System.out.println("Соединение закрыто");
-                    socket.close();
+                    clientSocket.close();
+                } catch (Exception ex) {
+                    // ignore
                 }
             }
-        } finally {
-            server.close();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int serverPort = 1234;
+        ServerSocket serverSocket = new ServerSocket(serverPort);
+        System.out.println("Старт сервера порта : " + serverPort);
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Клиент подключился");
+                threadPool.submit(new Server.Handler(clientSocket));
+            } catch (IOException e) {
+                System.out.println("Соединение не устанавливается");
+                System.exit(-1);
+            }
         }
     }
 }
